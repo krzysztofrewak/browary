2bf9862d4888bb02cc71d8ed098f0dd2ef12db38
krzysztofrewak <10898728+krzysztofrewak@users.noreply.github.com>
2020-10-12 17:45:04 +0000

    Deploying to public from  @ 54ee649db33c0ba94f231b0d4a4395ef0582ab1e 🚀

--- frontend/components/views/statistics/CompassRow.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="flex h-24 w-full items-center text-center">
-    <slot></slot>
-  </div>
-</template>
--- frontend/resources/Resource.js
+++
@@ -1,9 +0,0 @@
-class Resource {
-    static fetchFrom(url, callback) {
-        return fetch(url).then(response => response.json()).then(result => {
-            callback(result)
-        })
-    }
-}
-
-export default Resource
