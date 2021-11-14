fcabe037ee1b6e52861195a8a562a7603703a5b1
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2021-11-14 14:55:31 +0100

    zachodniopomorskie, listopad 2021

--- frontend/components/Counters.vue
+++ frontend/components/Counters.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-4 my-4 text-center flex justify-center">
+  <div class="px-4 my-4 text-center grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4">
     <slot></slot>
   </div>
 </template>
