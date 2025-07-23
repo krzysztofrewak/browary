9c8759d1194c08b79905023f106b09115b869de6
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2025-07-23 20:36:08 +0200

    wakacje 2025 (#95)

---
+++ backend/compare.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . "/../vendor/autoload.php";
+
+$publishedRaw = file_get_contents("https://browary.rewak.pl/api/statistics.json");
+$publishedData = json_decode($publishedRaw, associative: true);
+
+$raw = file_get_contents("./public/api/statistics.json");
+$data = json_decode($raw, associative: true);
+
+foreach (["breweries", "countries", "cities", "trips", "tags"] as $section) {
+    if ($data["counters"][$section] > $publishedData["counters"][$section]) {
+        $number = $data["counters"][$section] - $publishedData["counters"][$section];
+        echo "new $section: $number" . PHP_EOL;
+    }
+}
